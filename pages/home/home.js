bbab3bd780621956a7bcb146d0c17eda3b3bc916
stud <super1742720898@gmail.com>
2020-06-05 09:53:35 +0800

    :arrow_up:update project add swipe items

---
+++ pages/home/home.js
@@ -0,0 +1,14 @@
+Page({
+    data: {
+        message:'HelloWorld',
+        array:[1,2,3,4,5],
+        view:'HELLO',
+        staff1:{firstName:'zhang',lastName:'san'},
+        staff1:{firstName:'li',lastName:'si'},
+        staff1:{firstName:'wang',lastName:'wu'},
+
+    },
+    onLoad: function (options) {
+
+    }
+});
