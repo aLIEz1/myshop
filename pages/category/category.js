9714829f601a9a33cc69461089052f9fbd9292ca
lm <super1742720898@gmail.com>
2020-06-06 11:10:28 +0800

    :arrow_up: add category

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,6 +1,55 @@
 Page({
-    data: {},
-    onLoad: function (options) {
+    data: {
+        categories: [],//所有的类别信息
+        currentMenuIndex: 0,//当前选中的项
+        categoryInfo: {}
+    },
 
+    //获取类别信息
+    getCategorys: function () {
+        var url = 'http://qhejpxn.cn/api/categories';
+        wx.request({
+            url: url,
+            success: res => {
+                console.log(res)
+                var categoryData = res.data;
+                this.setData({
+                    categories: res.data
+                });
+                if (categoryData.length > 0) {
+                    this.getProductsByCategory(categoryData[0].id)
+                }
+            }
+        })
+    },
+    changeCategory: function (event) {
+        // console.log(event);
+        var index = event.currentTarget.dataset.index;
+        this.setData({
+            currentMenuIndex: index
+        });
+    },
+    getProductsByCategory: function (id) {
+        var url = 'http://qhejpxn.cn/api/categories/' + id;
+        var index = this.data.currentMenuIndex;
+        var categoryData = this.data.categories;
+        wx.request({
+            url,
+            success: res => {
+                console.log(res)
+                var dataObj = {
+                    topImgUrl: categoryData[index].img.url,
+                    title: categoryData[index].name,
+                    products: res.data.products,
+
+                };
+                this.setData({
+                    categoryInfo:dataObj,
+                });
+            }
+        })
+    },
+    onLoad: function (options) {
+        this.getCategorys();
     }
 });
